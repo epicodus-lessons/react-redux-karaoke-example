39031f05e0f0f7d6b38e266372c477ee9ff15bc6
Courtney Phillips <courtney@epicodus.com>
2017-09-14 12:53:24 -0700

    Fix error in title.

--- webpack.config.js
+++ webpack.config.js
@@ -81,7 +81,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template:'template.ejs',
       appMountId: 'react-app-root',
-      title: 'React Help Queue',
+      title: 'Redux Karaoke',
       filename: resolve(__dirname, "build", "index.html"),
     }),
   ]
