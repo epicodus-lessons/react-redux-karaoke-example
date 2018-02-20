8743aa494f625c4e1cf473eb1464508596211c9b
Courtney Phillips <courtney@epicodus.com>
2018-02-20 15:06:46 -0800

    Define restartSong action creator.

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,3 +4,8 @@ export const nextLyric = (currentSongId) => ({
   type: types.NEXT_LYRIC,
   currentSongId
 });
+
+export const restartSong = (currentSongId) => ({
+  type: types.RESTART_SONG,
+  currentSongId
+});
