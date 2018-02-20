04cffceed6696642a495a7a7f195e7cc7de2cbb4
Courtney Phillips <courtney@epicodus.com>
2018-02-20 14:59:52 -0800

    Use new nextLyric action creator in tests.

--- __tests__/reducers/index.js
+++ __tests__/reducers/index.js
@@ -3,6 +3,7 @@ import songChangeReducer from './../../src/reducers/songChangeReducer';
 import lyricChangeReducer from './../../src/reducers/lyricChangeReducer';
 import rootReducer from './../../src/reducers/';
 import { createStore } from 'redux';
+import * as actions from './../../src/actions';
 
 describe('Karaoke App', () => {
   const { initialState, types } = constants;
@@ -14,7 +15,7 @@ describe('Karaoke App', () => {
     });
 
     it('Should update currently-displayed lyric of song', () => {
-      expect(lyricChangeReducer(initialState.songsById, { type: 'NEXT_LYRIC', currentSongId: 2 })[2].arrayPosition).toEqual(initialState.songsById[2].arrayPosition + 1);
+      expect(lyricChangeReducer(initialState.songsById, actions.nextLyric(2))[2].arrayPosition).toEqual(initialState.songsById[2].arrayPosition + 1);
     });
 
     it('Should restart song', () => {
