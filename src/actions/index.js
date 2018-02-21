e3f171c398011fab59cc0b10eb5bc33bc0446c90
Courtney Phillips <courtney@epicodus.com>
2018-02-21 09:54:13 -0800

    Program fetchSongId() async action to dispatch a further requestSong() action to inform reducers request to API is in process.

--- src/actions/index.js
+++ src/actions/index.js
@@ -19,6 +19,7 @@ export const changeSong = (newSelectedSongId) => ({
 export function fetchSongId(title) {
   return function (dispatch) {
     const localSongId = v4();
+    dispatch(requestSong(title, localSongId));
     title = title.replace(' ', '_');
     return fetch('http://api.musixmatch.com/ws/1.1/track.search?&q_track=' + title + '&page_size=1&s_track_rating=desc&apikey=YOUR-API-KEY-HERE').then(
       response => response.json(),
