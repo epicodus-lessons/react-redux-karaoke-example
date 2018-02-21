55898405b700b7d55a2764d3275ea8da693a3877
Courtney Phillips <courtney@epicodus.com>
2018-02-21 09:58:25 -0800

    Write test for new requestSong() action.

--- __tests__/reducers/index.js
+++ __tests__/reducers/index.js
@@ -21,6 +21,18 @@ describe('Karaoke App', () => {
     it('Should restart song', () => {
       expect(lyricChangeReducer(initialState.songsById, actions.restartSong(1))[1].arrayPosition).toEqual(0);
     });
+
+    it('Should update state when API lyrics are being requested.', () => {
+      const action = actions.requestSong('crocodile rock');
+      const newStateEntry = {
+        isFetching: true,
+        title: action.title,
+        songId: action.songId,
+      };
+      expect(lyricChangeReducer(initialState.songsById, action)[action.songId])
+      .toEqual(newStateEntry);
+    });
+
   });
 
   describe('songChangeReducer', () => {
