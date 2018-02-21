02cbf3f0d1459c6d16cd2bfcd2bb1e616d9a1cb8
Courtney Phillips <courtney@epicodus.com>
2018-02-21 09:44:29 -0800

    Call changeSong action creator in SongList component.

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { restartSong, changeSong } from './../actions';
 
 const SongList = ({ dispatch, songList }) => {
   let action;
@@ -11,17 +12,9 @@ const SongList = ({ dispatch, songList }) => {
         let song = songList[songId];
         return <li key = {songId} onClick = {() => {
           if (song.arrayPosition > 0){
-            action = {
-              type: 'RESTART_SONG',
-              currentSongId: songId
-            };
-            dispatch(action);
+            dispatch(restartSong(songId));
           }
-          action = {
-            type: 'CHANGE_SONG',
-            newSelectedSongId: songId
-          };
-          dispatch(action);
+          dispatch(changeSong(songId));
         }}>
           {song.title} by {song.artist}</li>;
       })}
