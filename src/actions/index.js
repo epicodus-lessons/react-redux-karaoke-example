0b77a1c3f7d52bd329721674add7c973b1221b0f
Courtney Phillips <courtney@epicodus.com>
2018-02-21 09:52:03 -0800

    Define fetchSongId() async action to contact Musixmatch API.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
-import * as types from "./../constants/ActionTypes";
+import * as types from './../constants/ActionTypes';
+import v4 from 'uuid/v4';
 
 export const nextLyric = (currentSongId) => ({
   type: types.NEXT_LYRIC,
@@ -14,3 +15,16 @@ export const changeSong = (newSelectedSongId) => ({
   type: types.CHANGE_SONG,
   newSelectedSongId
 });
+
+export function fetchSongId(title) {
+  return function (dispatch) {
+    const localSongId = v4();
+    title = title.replace(' ', '_');
+    return fetch('http://api.musixmatch.com/ws/1.1/track.search?&q_track=' + title + '&page_size=1&s_track_rating=desc&apikey=YOUR-API-KEY-HERE').then(
+      response => response.json(),
+      error => console.log('An error occurred.', error)
+    ).then(function(json) {
+      console.log('CHECK OUT THIS SWEET API RESPONSE:', json)
+    });
+  };
+}
