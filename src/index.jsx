45b0b72eb5fb4705c791eeb3250403e7556a10ff
Courtney Phillips <courtney@epicodus.com>
2018-02-21 10:55:39 -0800

    Apply thunkMiddleware to Redux store in entry point file.

--- src/index.jsx
+++ src/index.jsx
@@ -5,8 +5,9 @@ import { Provider } from 'react-redux';
 import rootReducer from './reducers';
 import { createStore, applyMiddleware } from 'redux';
 import middlewareLogger from './middleware/middleware-logger';
+import thunkMiddleware from 'redux-thunk';
 
-const store = createStore(rootReducer, applyMiddleware(middlewareLogger));
+const store = createStore(rootReducer, applyMiddleware(middlewareLogger, thunkMiddleware));
 
 ReactDOM.render(
   <Provider store={store}>
