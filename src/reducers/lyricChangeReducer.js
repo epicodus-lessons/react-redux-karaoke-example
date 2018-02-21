4de53277ed92e5274546abb8d645dadd3228c433
Courtney Phillips <courtney@epicodus.com>
2018-02-21 11:42:11 -0800

    Update lyricChangeReducer to handle RECEIVE_SONG action types that parse relevant API data.

--- src/reducers/lyricChangeReducer.js
+++ src/reducers/lyricChangeReducer.js
@@ -32,6 +32,20 @@ const lyricChangeReducer = (state = initialState.songsById, action) => {
       [action.songId]: newSongsByIdEntry
     });
     return newSongsByIdStateSlice;
+  case types.RECEIVE_SONG:
+    newSongsByIdEntry = Object.assign({}, state[action.songId], {
+      isFetching: false,
+      receivedAt: action.receivedAt,
+      title: action.title,
+      artist: action.artist,
+      songArray: action.songArray,
+      arrayPosition: 0,
+      songId: action.songId
+    });
+    newSongsByIdStateSlice = Object.assign({}, state, {
+      [action.songId]: newSongsByIdEntry
+    });
+    return newSongsByIdStateSlice;
   default:
     return state;
   }
